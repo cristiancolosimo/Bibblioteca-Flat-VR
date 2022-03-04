6c8fc2398ed1a8fb26d2661ff81aced5651e31f5
cristian <santocristiancolosimo@gmail.com>
2022-03-04 20:29:48 +0100

    main VR entrypoint code

--- src/vr/VR.tsx
+++ src/vr/VR.tsx
@@ -4,32 +4,38 @@ import { useGLTF, useTexture } from "@react-three/drei";
 import { OrbitControls, Stars, } from '@react-three/drei'
 
 import Libro from '../../assets/model/libro/Libro'
-import Libreria from '../../assets/model/libreria/Libreria'
+import Bookself from '../../assets/model/libreria/Libreria'
 import { DefaultXRControllers, Hands, RayGrab, VRCanvas } from '@react-three/xr';
-
+import Player from './components/Player';
+import BackgroundWorld from './components/Background';
+import Room1 from './rooms/Room1';
 
 export default function VR(){
     return (
         <div style={{ height: "1000px", width: "100%", position: "relative", background: "#333" }}>
-          <Suspense fallback={null}>
-    
-    
+          <Suspense fallback={<span>Caricamento assets...</span>} >
             <VRCanvas >
-              <Stars />
-              <OrbitControls></OrbitControls>
-              <ambientLight intensity={0.5} />
-              <spotLight position={[10, 15, 10]} angle={0.3} />
-              <Hands />
-              <DefaultXRControllers />
+                <spotLight position={[10, 4, 10]} angle={0.3} />
+                <Player/>
+                <BackgroundWorld/>
+                <Room1></Room1>
+            </VRCanvas >
+          </Suspense>
     
-              {
+        </div>
+      )
+}
+
+/**
+ * 
+ * {
                 Array(2).fill(null).map((e, k) => {
                   let m = k;
                  // if(k > 3) m = k-3;
     
                   return (
-                    <mesh position={[0, -2 * m, 0]}>
-                      <Libreria />
+                    <mesh position={[0, 2 * m, 0]} rotation={[0,-Math.PI/2,0]}>
+                      <Bookself />
                       {
                         Array(24).fill(null).map((e, k) => {
                           let l = k;
@@ -44,9 +50,5 @@ export default function VR(){
                   );
                 })
               }
-            </VRCanvas >
-          </Suspense>
-    
-        </div>
-      )
-}
+
+ */
